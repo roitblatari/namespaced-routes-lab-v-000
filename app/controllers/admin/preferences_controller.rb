423b9d1b9685ec46195ac1c8cee97f347e85eebf
roitblatari <roitblatari@gmail.com>
2018-06-05 21:19:48 -0400

    Done.

--- app/controllers/admin/preferences_controller.rb
+++ app/controllers/admin/preferences_controller.rb
@@ -1,5 +1,15 @@
 class Admin::PreferencesController < ApplicationController
+  
   def index
-
+    @preference = Preference.first_or_create(allow_create_artists: true, allow_create_songs: true, song_sort_order: "DESC", artist_sort_order: "DESC")
+  # binåding.pry
   end
+
 end
+
+
+
+# class Admin::PreferencesController < ApplicationController
+#   def index
+    
+#   end
